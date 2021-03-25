b1e2b2fa02e79a7a93a3dde6c5b0f1da045be1a0
Amary <laumiqv@gmail.com>
2021-03-25 21:02:23 +0300

    init

---
+++ umi_work/assets/js/common.js
@@ -0,0 +1,10 @@
+  $(document).ready(function() {
+  $(".fancybox").fancybox({
+    padding: 0,
+    openEffect	: 'none',
+    closeEffect	: 'none', 
+    nextEffect: 'none', 
+    prevEffect:'none',
+    nextClick: true,
+  });
+  });
